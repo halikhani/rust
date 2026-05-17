5a11cbdb10d8bee8e1cf5455dfc60e1f7496d1ac
Hamidreza Alikhani <ha.alikhani.k@gmail.com>
2026-05-17 16:03:11 -0700

    basic workspace and lib connections

---
+++ projects/01/lib_hello/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn greeting() -> &'static str {
+    "Hello world!"
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
