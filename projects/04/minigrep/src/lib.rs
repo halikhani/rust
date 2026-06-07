7c92bc8bddcca869f955bc69c77410a4b9b564e7
Hamidreza Alikhani <ha.alikhani.k@gmail.com>
2026-06-06 18:06:03 -0700

    TDD with case sensitive bool for grep

--- projects/04/minigrep/src/lib.rs
+++ projects/04/minigrep/src/lib.rs
@@ -1,3 +1,51 @@
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec[&'a str] {
-    unimplemented!();
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // note on lifetimes: we basically say set the lifetime ofthe return value to be the same as the lifetime of the contents parameter
+    // since we are gonna return a slice of the contents, we need to tell the compiler that the return value will live as long as the contents parameter does
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase(); // NOTE: new query is owned object so we call contains on it with the &
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    }
 }
--- projects/04/minigrep/src/main.rs
+++ projects/04/minigrep/src/main.rs
@@ -3,13 +3,13 @@ use std::fs;
 use std::process;
 use std::error::Error;
 
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // let config = Config::new(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -23,15 +23,21 @@ fn main() {
 
 }
 
-struct Config {
-    query: String,
-    file_path: String,
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+    
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    // println!("With text:\n{contents}");
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
         println!("{line}");
     }
     Ok(())
@@ -44,7 +50,9 @@ impl Config {
         }
         let query = arg[1].clone();
         let file_path = arg[2].clone();
-        Ok(Config { query, file_path })
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok(); // is_ok() to see if the environment variable is set (returns bool)
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
